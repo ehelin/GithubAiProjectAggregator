1d12c37148ce8951f252813aef82ca3955d873eb
Eric Helin <erichelinse@gmail.com>
2025-10-31 10:57:16 -0500

    removing old code

--- Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GithubAiProjectAggregator.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
